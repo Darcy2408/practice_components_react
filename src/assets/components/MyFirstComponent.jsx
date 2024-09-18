95948363e1a8896b139e3f156d22771b600c5286
Darcy Novoa <darci-novoa99@hotmail.com>
2024-09-17 20:08:22 -0500

    My First Component

---
+++ src/assets/components/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+        <div>My First Components</div>
+        <h1>Temas de React</h1>
+        <ul>
+            <li>Components</li>
+            <li>Eventos</li>
+            <li>Estados Hooks</li>
+            <li>Props</li>
+        </ul>
+    </>
+    
+  )
+}
