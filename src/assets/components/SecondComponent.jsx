8a6a78f093092379dbc8e38ac1986105ccfd5ee1
Darcy Novoa <darci-novoa99@hotmail.com>
2024-11-09 15:36:28 -0500

    O

--- src/assets/components/SecondComponent.jsx
+++ src/assets/components/SecondComponent.jsx
@@ -1,37 +1,35 @@
-
 export const SecondComponent = () => {
+
     const books = [
-        "Harry Potter y la piedra filosofal",
-        "Harry Potter y la cámara secreta",
-        "Harry Potter y el prisionero de Azkaban",
-        "Harry Potter y el cáliz de fuego",
-        "Harry Potter y la Orden del Fénix",
-        "Harry Potter y el ministerio del Principe",
-        "Harry Potter y las Reliqueas de la Muerte",
-        "Animales fantasticos y donde encontrarlos",
-        "Otro libro"
+      "Harry Potter y la piedra filosofal",
+      "Harry Potter y la cámara secreta",
+      "Harry Potter y el prisionero de Azkaban",
+      "Harry Potter y el cáliz de fuego",
+      "Harry Potter y la Orden del Fénix",
+      "Harry Potter y el misterio del Príncipe",
+      "Harry Potter y las Reliquias de la Muerte",
+      "Animales Fantásticos y dónde encontrarlos",
+      "Otro libro"
     ];
-
-    const listBooks = [];
-    books.forEach((book, index) => {
-        listBooks.push(<li key={index}> 
-        {book}</li>)
-    })
-
-  return (
-    <>
-    <h1>SecondComponent</h1>
-    <div>
-        <ul>
-           {
-            books.map((book, index) => {
-                return <li key={ index}> 
-                {book} </li>
-            })
-           }
-        </ul>
-        <ul>{listBooks}</ul>
-    </div>
-    </>
-  )
-}
+  
+  
+    return (
+      <>
+        <h1>SecondComponent</h1>
+        <div>
+          <ul>
+            {books.length >= 1 ?
+              (
+                  books.map((book, index) => {
+                    return <li key={ index }> { book } </li>
+                  })
+              ) :
+              (
+                <p>No existen libros</p>
+              )
+            }
+          </ul>
+        </div>
+      </>
+    )
+  }
